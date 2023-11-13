6c518d072002886a14b44a485c8e00697ea5c4e0
biggeryao <62866598+biggeryao@users.noreply.github.com>
2023-11-13 16:48:54 +0800

    feat: 配置路由

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,18 @@
 import React, { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home: FC = () => {
-  return <h3>home</h3>
+  const nav = useNavigate()
+  function clickHandler() {
+    nav('/login')
+  }
+  return (
+    <div>
+      <h3>home</h3>
+      <div>
+        <button onClick={clickHandler}>登录</button>
+      </div>
+    </div>
+  )
 }
 export default Home
---
+++ src/router/index.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+
+import MainLayout from '../layouts/MainLayout'
+import ManageLayout from '../layouts/ManageLayout'
+import QuestionLayout from '../layouts/QuestionLayout'
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
+import NotFound from '../pages/NotFound'
+import List from '../pages/manage/List'
+import Trash from '../pages/manage/Trash'
+import Star from '../pages/manage/Star'
+import Edit from '../pages/question/Edit'
+import Stat from '../pages/question/Stat'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    children: [
+      { path: '/', element: <Home></Home> },
+      { path: 'login', element: <Login></Login> },
+      { path: 'register', element: <Register></Register> },
+      {
+        path: 'manage',
+        element: <ManageLayout />,
+        children: [
+          { path: 'list', element: <List></List> },
+          { path: 'star', element: <Star></Star> },
+          { path: 'trash', element: <Trash></Trash> },
+        ],
+      },
+      {
+        path: 'question',
+        element: <QuestionLayout></QuestionLayout>,
+        children: [
+          { path: 'edit/:id', element: <Edit></Edit> },
+          { path: 'stat/:id', element: <Stat></Stat> },
+        ],
+      },
+      { path: '*', element: <NotFound></NotFound> },
+    ],
+  },
+])
+
+export default router
