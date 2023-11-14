2e1dc1ce3918fd5c3b766d84f0ded3b3fe10a775
biggeryao <62866598+biggeryao@users.noreply.github.com>
2023-11-14 10:42:08 +0800

    refactor: antd重构MainLayout组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import routerConfig from './router'
+import 'yao-react-ui/dist/index.css'
+
 function App() {
   return <RouterProvider router={routerConfig}></RouterProvider>
 }
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
-
+import { Layout } from 'antd'
+const { Header, Footer, Content } = Layout
+import styles from './MainLayout.module.scss'
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout>
+      <Header className={styles.header}>
+        <div className={styles.left}>logo</div>
+        <div className={styles.right}>用户信息</div>
+      </Header>
+      <Content className={styles.main}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>问卷 2023</Footer>
+    </Layout>
   )
 }
 export default MainLayout
