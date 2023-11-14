c6ffa765cbf929aab79cf81217496aba11208c74
biggeryao <62866598+biggeryao@users.noreply.github.com>
2023-11-14 11:50:30 +0800

    feat: manageLayout组件

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,11 +3,14 @@ import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd'
 const { Header, Footer, Content } = Layout
 import styles from './MainLayout.module.scss'
+import Logo from '../components/Logo'
 const MainLayout: FC = () => {
   return (
     <Layout>
       <Header className={styles.header}>
-        <div className={styles.left}>logo</div>
+        <div className={styles.left}>
+          <Logo></Logo>
+        </div>
         <div className={styles.right}>用户信息</div>
       </Header>
       <Content className={styles.main}>
--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,14 +1,49 @@
 import React, { FC } from 'react'
 import { Outlet } from 'react-router-dom'
 import styles from './Manage.module.scss'
+import { Button } from 'yao-react-ui'
+import { Space } from 'antd'
+import { useNavigate, useLocation } from 'react-router-dom'
 const ManageLayout: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
   return (
     <div className={styles.container}>
-      <div className={styles.left}>ManageLayout left</div>
-      <div>
-        <Outlet />
+      <Space className={styles.left} direction="vertical">
+        <Button btnType="primary" size="lg">
+          创建问卷
+        </Button>
+        <Button
+          btnType={pathname.startsWith('/manage/list') ? 'primary' : 'default'}
+          size="lg"
+          onClick={() => {
+            nav('/manage/list')
+          }}
+        >
+          我的问卷
+        </Button>
+        <Button
+          btnType={pathname.startsWith('/manage/star') ? 'primary' : 'default'}
+          size="lg"
+          onClick={() => {
+            nav('/manage/star')
+          }}
+        >
+          星标问卷
+        </Button>
+        <Button
+          btnType={pathname.startsWith('/manage/trash') ? 'primary' : 'default'}
+          size="lg"
+          onClick={() => {
+            nav('/manage/trash')
+          }}
+        >
+          回收站
+        </Button>
+      </Space>
+      <div className={styles.right}>
+        <Outlet></Outlet>
       </div>
-      <div className={styles.right}>ManageLayout right</div>
     </div>
   )
 }
